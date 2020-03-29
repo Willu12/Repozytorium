c4a4a38bfa32eef6efd34f00dfd510799d140634
Willu12 <62487003+Willu12@users.noreply.github.com>
2020-03-30 00:05:24 +0200

    zjebane torchje

--- Tojesttenmoment/Assets/_Scripts/BallManager.cs
+++ Tojesttenmoment/Assets/_Scripts/BallManager.cs
@@ -6,21 +6,36 @@ public class BallManager : MonoBehaviour
 {
     // Start is called before the first frame update
     Rigidbody r;
+    bool hit;
     void Start()
     {
+        hit = false;
         r = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(hit);
     }
     private void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag == "Wall")
         {
-            r.AddForce(new Vector3(3,1,0));
+            r.AddForce(new Vector3(0.7f,0.5f,0));
+            hit = false;
+        }
+        if (col.gameObject.tag == "Player")
+        {
+            hit = true;
         }
     }
+    private void OnMouseDown()
+    {
+        if (hit)
+        {
+            r.AddForce(new Vector3(-0.7f, 0.4f, 0));
+        }
+    }
+
 }
