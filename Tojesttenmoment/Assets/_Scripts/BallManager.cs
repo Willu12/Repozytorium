1cc4c9b75720b3095df0a85e96cb576ab6eae4aa
McReseT <62754516+McReseT@users.noreply.github.com>
2020-04-01 09:52:51 +0200

    Score, gravity fix
    Features

--- Tojesttenmoment/Assets/_Scripts/BallManager.cs
+++ Tojesttenmoment/Assets/_Scripts/BallManager.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BallManager : MonoBehaviour
 {
     // Start is called before the first frame update
     Rigidbody rb;
     bool hit;
-    public GameObject enemy, ally;
-    float boost = 1f, slow=1f;
+    public GameObject enemy, ally, tmp;
+    private TextMeshProUGUI textMesH;
+    float change = 1f;
+    int points=0;
     void Start()
     {
         hit = false;
         rb = GetComponent<Rigidbody>();
+        textMesH = tmp.GetComponent<TextMeshProUGUI>();
+        textMesH.text = points.ToString();
+
+        rb.angularVelocity = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f)*10;
+        rb.useGravity = false;
     }
 
     // Update is called once per frame
@@ -25,9 +33,9 @@ public class BallManager : MonoBehaviour
         if (col.gameObject.tag == "Wall")
         {
             rb.velocity = Vector3.zero;
-            rb.AddForce(new Vector3(1.5f*boost,-0.3f*slow,Random.Range(-0.1f, +0.1f)));
-            boost += 0.025f;
-            slow += 0.025f;
+            rb.AddForce(new Vector3(1.5f*change,-0.3f*change, Random.Range(-0.1f, +0.1f)));
+            Physics.gravity = new Vector3(change, change, 1f);
+            change += 0.025f;
             hit = false;
             ally.SetActive(true);
             enemy.SetActive(false);
@@ -43,9 +51,15 @@ public class BallManager : MonoBehaviour
     {
         if (hit)
         {
+            if(rb.useGravity == true)
+            {
+                points++;
+                textMesH.text = points.ToString();
+            }
+            rb.useGravity = true;
             rb.velocity = Vector3.zero;
-            rb.AddForce(new Vector3(-1.5f*boost,-0.3f*slow, Random.Range(-0.1f, +0.1f)));
+            rb.AddForce(new Vector3(-1.5f*change, -0.3f*change, Random.Range(-0.1f, +0.1f)));
+            ally.SetActive(false);
         }
     }
-
 }
