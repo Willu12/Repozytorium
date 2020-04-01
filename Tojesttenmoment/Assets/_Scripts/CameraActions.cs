15fb318e65479d546947128309ea2ea0ae5fa911
McReseT <62754516+McReseT@users.noreply.github.com>
2020-04-01 23:45:09 +0200

    CAMERA
    CAMERA MOVEMENT

---
+++ Tojesttenmoment/Assets/_Scripts/CameraActions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraActions : MonoBehaviour
+{
+    public Transform target, ball;
+
+    public float smoothSpeed = 0.075f;
+    public Vector3 offset;
+
+    private Vector3 desiredPosition;
+
+    void FixedUpdate()
+    {
+        if (ball.transform.position.x > -1.5f)
+        {
+            desiredPosition = target.position + offset;
+        } else desiredPosition = new Vector3(target.position.x, target.position.y, 0f) + offset;
+
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    
+        transform.LookAt(target);
+    }
+}
---
+++ Tojesttenmoment/Assets/_Scripts/Mimic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mimic : MonoBehaviour
+{
+    public GameObject source;
+    public bool x, y, z;
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (x == true)
+            transform.position = new Vector3(source.transform.position.x, transform.position.y, transform.position.z);
+        if (y == true)
+            transform.position = new Vector3(transform.position.x, source.transform.position.y, transform.position.z);
+        if (z == true)
+            transform.position = new Vector3(transform.position.x, transform.position.y, source.transform.position.z);
+    }
+}
